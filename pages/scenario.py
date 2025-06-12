9df64175b614dd23ff6fbb733ab18c2d1b62e5e9
Tony Bui <tony.bui@rea-group.com>
2025-06-12 15:56:48 +1000

    Use the enterprise version

--- pages/scenario.py
+++ pages/scenario.py
@@ -31,6 +31,9 @@ def create_layout():
             html.Button("Download CSV Display", id="csv-button", n_clicks=0),
             AgGrid(
                 id="scenario_grid",
+                enableEnterpriseModules=True,
+                 dashGridOptions={"enableRangeSelection":True},
+      licenseKey="[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-087409}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{30 June 2025}____[v3]_[0102]_MTc1MTIzODAwMDAwMA==e234b19b68957c055f5bc88abec0f85f",
                 rowData=product_specifications.to_dict("records"),
                 columnDefs=[
                     {"field": "On/Off"},
